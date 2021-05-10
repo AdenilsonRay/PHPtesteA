dbf427b43b2c8ce077e57aa4867d42dab2a53c83
brumatt <robsonbrumatt@gmail.com>
2021-05-10 10:08:12 -0300

    finalizado pedido

--- Regras/Pedidos/cadastra_pedido.php
+++ Regras/Pedidos/cadastra_pedido.php
@@ -4,28 +4,35 @@ $nome_produto=FILTER_INPUT(INPUT_POST,'nome_produto',FILTER_DEFAULT);
 $qtd_produto=filter_input(INPUT_POST,'qtd_produto',FILTER_DEFAULT);
 $obs_produto=filter_input(INPUT_POST,'obs_produto',FILTER_DEFAULT);
 $preco_produto=filter_input(INPUT_POST,'preco_produto',FILTER_DEFAULT);
+$cod_usuario=1;
 
 //Criando conexao com o banco
 require_once('../../BDA/conexao.php');
 
 //Preparando sql 
-$Consulta = $conn->prepare("insert into item_pedido (nome_produto,quantidade,preco_und,observacao,cod_usuario,data_hora) values (':nome_produto',':quamtidade',':preco_und',':observacao',':cod_usuario',':data_hora')");
-$Consulta->bindParam(':nome_produto',$nome_produto,PDO::PARAM_STR);
-$Consulta->bindParam(':qtd_produto',$qtd_produto,PDO::PARAM_INT);
-$Consulta->bindParam(':obs_produto',$obs_produto,PDO::PARAM_STR);
-$Consulta->bindParam(':preco_produto',$preco_produto,PDO::PARAM_INT);
+$Consulta = $conn->prepare("insert into item_pedido (nome_produto,quantidade,preco_und,observacao,cod_usuario) "
+                                          . "values ('$nome_produto','$qtd_produto','$preco_produto','$obs_produto','$cod_usuario')");
+//$Consulta->bindParam(':nome_produto',$nome_produto,PDO::PARAM_STR);
+//$Consulta->bindParam(':qtd_produto',$qtd_produto,PDO::PARAM_INT);
+//$Consulta->bindParam(':preco_produto',$preco_produto,PDO::PARAM_STR); 
+//$Consulta->bindParam(':obs_produto',$obs_produto,PDO::PARAM_STR);
 
-//Executa query
-$Consulta->execute();
 
-//Recupera informacao
-$resultado=$Consulta->fetchAll();
+try {
+    //Executa query
+    $Consulta->execute();
 
-if (count($resultado)>0) {
-    # code...
-}else{
-
+    $resultado['cod']=1;
+    $resultado['msg']='Item inserido com sucesso!';
+    $resultado['style']="alert-success";
+    
+} catch (\Throwable $th) {
+    $resultado['cod']=0;
+    $resultado['msg']= $th->getMessage().'Item não inserido com sucesso!';
+    $resultado['style']="alert-danger";
 }
+
+require_once '../../index.php';
 
 
 
--- index.php
+++ index.php
@@ -4,23 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de pedidos 1.0</title>
+    <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Efetue login</h2>
-    <form id="form_loginm" action="Interfaces/Login/login.php" method="POST">
-        <?php if(isset($resultado) && $resultado['cod']==0): ?>
-            <div class="alert alert-danger">
-            <?php echo $resultado['msg']; ?>
+    <div class="container">
+        <form action="Regras/Pedidos/cadastra_pedido.php" method="POST">
+        <h2>Escolha de iten do pedido</h2><br>
+            <div class="form-group">
+                <label for="nome_produto">Nome do Produto</label>
+                <input type="text" class="form-control" name="nome_produto" id="nome_produto" aria-describedby="emailHelp" placeholder="Digite o produto" required>
+            </div>      
+            <div class="form-group">
+                <label for="qtd_produto">Quantidade</label>
+                <input type="number" class="form-control" name="qtd_produto" id="qtd_produto" aria-describedby="emailHelp" required maxlength="10">
             </div>
-        <?php endif; ?>
-        <input type="email" id="email" name="email" placeholder="Digite seu email!" required>  <br><br>
-        <input type="password" id="senha" name="senha" placeholder="Digite sua senha" required ><br><br>
-        <input type="submit" id="submeter" value="Entrar" class="btn btn-primary">
-    </form>
+            <div class="form-group">
+                <label for="obs_produto">Observação</label>
+                <input type="text" class="form-control" name="obs_produto" id="obs_produto" aria-describedby="emailHelp" placeholder="Digite as observações">
+            </div>      
+            <div class="form-group">
+                <label for="preco_produto">Preço unitário</label>
+                <input type="text" class="form-control" name="preco_produto" id="preco_produto" aria-describedby="emailHelp" required>
+            </div> 
+            <button type="submit" class="btn btn-primary">Adicionar itens</button>
+
+            <?php if(isset($resultado)): ?>
+                <div class="alert <?= $resultado['style'] ?>">
+                    <?php echo $resultado["msg"] ?>                
+                </div>   
+            <?php endif; ?>
+        </form>  
+    </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </html>
